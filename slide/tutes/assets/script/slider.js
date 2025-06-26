f53c99faedca7e40e2353bbed7ed0366876d8919
almuhdilkarim <almuhdilkarim2@mail.com>
2025-06-27 00:22:05 +0700

    update

--- slide/tutes/assets/script/slider.js
+++ slide/tutes/assets/script/slider.js
@@ -39,88 +39,23 @@ inputForm.onsubmit = (event) => {
     });
 
 
-// ArrayBuffer
- ttsRecorder.start()
- // `tts` : `SpeechSynthesisRecorder` instance, `data` : audio as `dataType` or method call result
- .then(tts => tts.arrayBuffer())
- .then(({tts, data}) => {
-   // do stuff with `ArrayBuffer`, `AudioBuffer`, `Blob`,
-   // `MediaSource`, `MediaStream`, `ReadableStream`
-   // `data` : `ArrayBuffer`
-   tts.audioNode.src = URL.createObjectURL(new Blob([data], {type:tts.mimeType}));
-   tts.audioNode.title = tts.utterance.text;
-   tts.audioNode.onloadedmetadata = () => {
-     console.log(tts.audioNode.duration);
-     tts.audioNode.play();
-   }
- })
- // AudioBuffer     
- ttsRecorder.start()
- .then(tts => tts.audioBuffer())
- .then(({tts, data}) => {
-   // `data` : `AudioBuffer`
-   let source = tts.audioContext.createBufferSource();
-   source.buffer = data;
-   source.connect(tts.audioContext.destination);
-   source.start()
- })
- // Blob
- ttsRecorder.start()
- .then(tts => tts.blob())
- .then(({tts, data}) => {
-   // `data` : `Blob`
-   tts.audioNode.src = URL.createObjectURL(blob);
-   tts.audioNode.title = tts.utterance.text;
-   tts.audioNode.onloadedmetadata = () => {
-     console.log(tts.audioNode.duration);
-     tts.audioNode.play();
-   }
- })
- // ReadableStream
- ttsRecorder.start()
- .then(tts => tts.readableStream())
- .then(({tts, data}) => {
-   // `data` : `ReadableStream`
-   console.log(tts, data);
-   data.getReader().read().then(({value, done}) => {
-     tts.audioNode.src = URL.createObjectURL(value[0]);
-     tts.audioNode.title = tts.utterance.text;
-     tts.audioNode.onloadedmetadata = () => {
-       console.log(tts.audioNode.duration);
-       tts.audioNode.play();
-     }
-   })
- })
- // MediaSource
- ttsRecorder.start()
- .then(tts => tts.mediaSource())
- .then(({tts, data}) => {
-   console.log(tts, data);
-   // `data` : `MediaSource`
-   tts.audioNode.srcObj = data;
-   tts.audioNode.title = tts.utterance.text;
-   tts.audioNode.onloadedmetadata = () => {
-     console.log(tts.audioNode.duration);
-     tts.audioNode.play();
-   }
- })
- // MediaStream
- let ttsRecorder = new SpeechSynthesisRecorder({
-   text: "The revolution will not be televised", 
-   utternanceOptions: {
-     voice: "english-us espeak",
-     lang: "en-US",
-     pitch: .75,
-     rate: 1
-   }, 
-   dataType:"mediaStream"
- });
- ttsRecorder.start()
- .then(({tts, data}) => {
-   // `data` : `MediaStream`
-   // do stuff with active `MediaStream`
- })
- .catch(err => console.log(err))
+// Blob
+ttsRecorder
+  .start()
+  .then(tts => {
+    return tts.audioBuffer();
+  })
+  .then(({ tts, data }) => {
+    console.log(data);
+    const wav = bufferToWav(data);
+    const blob = new window.Blob([new DataView(wav)], {
+      type: "audio/wav"
+    });
+    audioEncoder(blob, 128, null, function onComplete(blob) {
+      saveAs(blob, "sound.mp3");
+    });
+  });
+
 
 
 };
