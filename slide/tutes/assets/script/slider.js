df8b8521fa02081b317a661f65368411500df81e
almuhdilkarim <almuhdilkarim2@mail.com>
2025-06-26 22:58:35 +0700

    update

---
+++ slide/tutes/assets/script/slider.js
@@ -0,0 +1,60 @@
+const synth = window.speechSynthesis;
+
+const inputForm = document.querySelector("form");
+const inputTxt = document.getElementById("speakup");
+const voiceSelect = document.querySelector("select");
+synth.cancel();       
+let voices;
+
+function loadVoices() {
+  voices = synth.getVoices();
+  for (const [i, voice] of voices.entries()) {
+    const option = document.createElement("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+    option.value = i;
+    voiceSelect.appendChild(option);
+  }
+}
+
+// in Google Chrome the voices are not ready on page load
+if ("onvoiceschanged" in synth) {
+  synth.onvoiceschanged = loadVoices;
+} else {
+  loadVoices();
+}
+
+inputForm.onsubmit = (event) => {
+  event.preventDefault();
+
+
+    let ttsRecorder = new SpeechSynthesisRecorder({
+    text: inputTxt.value, 
+    utteranceOptions: {
+        voice: "english-us espeak",
+        lang: "en-US",
+        pitch: .75,
+        rate: 1,
+        volume: 50
+    }
+    });
+
+
+    ttsRecorder.start()
+      .then(tts => tts.blob())
+      .then(({
+        tts, data
+      }) => {
+        // do stuff with `ArrayBuffer`, `AudioBuffer`, `Blob`, `MediaSource`, `MediaStream`, `ReadableStream`
+        console.log(tts, data);
+          tts.audioNode.src = URL.createObjectURL(data);
+          tts.audioNode.title = tts.utterance.text;
+          tts.audioNode.onloadedmetadata = () => {
+            console.log(tts.audioNode.duration);
+            tts.audioNode.play();
+          }
+      })
+
+
+
+};
+
